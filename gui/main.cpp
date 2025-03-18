200d1798b50683047494fc20603d5110b6f1dc9d
Colin Willard <107717368+cwillar1@users.noreply.github.com>
2025-03-18 01:02:32 +0000

    gtk framework

---
+++ gui/main.cpp
@@ -0,0 +1,9 @@
+#include <gtk/gtk.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+}
--- install.sh
+++ install.sh
@@ -11,10 +11,20 @@ docker ps -f name=abg_container
 
 # Env bioinformatics packages
 # List of bioinformatics packages to check and install if not present
-packages=("spades" "fastqc")
+packages=("spades" "fastqc" "git" "python3-matplotlib" "pkg-config" "libfreetype6-dev" "libpng-dev" "python3" "python-setuptools")
 
 for package in "${packages[@]}"; do
     if ! command -v $package &> /dev/null; then
         sudo apt install -y $package
+    else
+        echo "$package is already installed."
     fi
 done
+
+# Install quast
+#git clone https://github.com/ablab/quast.git
+#bash quast/install.sh
+
+# prokka container
+docker pull staphb/prokka:latest
+docker run staphb/prokka:latest prokka -h
