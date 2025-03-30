2efdaeace340e745bc4293af3f2cf08174ba8344
Colin Willard <107717368+cwillar1@users.noreply.github.com>
2025-03-30 01:06:00 +0000

    gtk implem

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,9 +1,32 @@
 #include <gtk/gtk.h>
-#include <string.h>
-#include <iostream>
 
-using namespace std;
+// Callback function for button click event
+static void on_button_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Button clicked!\n");
+}
 
-int main(){
-    cout << "Hello World" << endl;
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "GTK 3.0 Example");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    //button
+    GtkWidget *button = gtk_button_new_with_label("Click Me");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+
+    // container
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(box), button);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    gtk_widget_show_all(window);
+
+    // Start the main GTK loop
+    gtk_main();
+
+    return 0;
 }
